0030303fb365be3f949bdcbe7cfa6a123ee4c208
Rakesh <polepalle.rakesh@capgemini.com>
2025-04-23 11:58:17 +0530

    second commit

--- Selinium/src/com/example/Basic.java
+++ Selinium/src/com/example/Basic.java
@@ -3,16 +3,18 @@ package com.example;
 public class Basic {
 
 	public static void main(String[] args) {
-		int i=10;
+		int i = 10;
 		System.out.println(i);
-		Basic b=new Basic();
-		
-		System.out.println(b.add(10,20));
-		
+		Basic b = new Basic();
+
+		System.out.println(b.add(10, 20));
+		System.out.println("doing some changes");
 
 	}
-public int add(int a,int b) {
-	int c=a+b;
-	return c;
-}
+
+	public int add(int a, int b) {
+		int c = a + b;
+		return c;
+
+	}
 }
