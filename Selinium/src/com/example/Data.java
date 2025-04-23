9d09b326ede39afc843dac6c03023c3b14e06682
Rakesh <polepalle.rakesh@capgemini.com>
2025-04-23 12:03:17 +0530

    first

---
+++ Selinium/src/com/example/Data.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class Data {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 10;
+		System.out.println(i);
+		Basic b = new Basic();
+
+		System.out.println(b.add(10, 20));
+		System.out.println("doing some changes");
+
+	}
+
+}
