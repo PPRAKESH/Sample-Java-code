74a85f133f0833ac40705f5280c311892c973f8e
Rakesh <polepalle.rakesh@capgemini.com>
2025-04-23 11:50:31 +0530

    first commit

---
+++ Selinium/src/com/example/Basic.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		int i=10;
+		System.out.println(i);
+		Basic b=new Basic();
+		
+		System.out.println(b.add(10,20));
+		
+
+	}
+public int add(int a,int b) {
+	int c=a+b;
+	return c;
+}
+}
